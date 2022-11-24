e33cb4d5fe22351f1e23d28a1d0e8b9fe14bb1b0
johnswekenyi <johnswekenyi@gmail.com>
2022-11-24 23:42:41 +0300

    Add customer tests

--- src/main/java/BaseClass.java
+++ src/main/java/BaseClass.java
@@ -9,6 +9,11 @@ public class BaseClass {
     public void setUp() {
         driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
         driver.manage().window().maximize();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
     @After
     public void tearDown() {
--- src/test/java/BankManagerTest.java
+++ src/test/java/BankManagerTest.java
@@ -1,14 +1,25 @@
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 
 public class BankManagerTest extends BaseClass {
     @Test
-    public void loginAsBankManagerTest() {
-
+    public void loginAsBankManagerTest() throws InterruptedException {
+        driver.findElement(By.xpath("//button[contains(text(), 'Bank Manager Login')]")).click();
+        Thread.sleep(2000);
     }
+    @Test()
+    public void addNewCustomerTest() throws InterruptedException {
+        loginAsBankManagerTest();
+        driver.findElement(By.xpath("//button[contains(text(), 'Add Customer\n" +
+                "\t\t')]")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("input[placeholder='First Name']")).sendKeys("Test");
+        driver.findElement(By.cssSelector("input[placeholder='Last Name']")).sendKeys("Last");
+        driver.findElement(By.cssSelector("input[placeholder='Post Code']")).sendKeys("00100");
+        driver.findElement(By.cssSelector(".btn-default")).click();
 
-    @Test
-    public void addNewCustomerTest(){
-
+        Thread.sleep(2000);
     }
 
     @Test
