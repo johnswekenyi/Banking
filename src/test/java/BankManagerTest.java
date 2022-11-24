d129d1ce26d0d55ff04e63a5b556bab7fbee3840
johnswekenyi <johnswekenyi@gmail.com>
2022-11-25 00:23:55 +0300

    Add account

--- src/test/java/BankManagerTest.java
+++ src/test/java/BankManagerTest.java
@@ -1,6 +1,10 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class BankManagerTest extends BaseClass {
     @Test
@@ -19,12 +23,20 @@ public class BankManagerTest extends BaseClass {
         driver.findElement(By.cssSelector("input[placeholder='Post Code']")).sendKeys("00100");
         driver.findElement(By.cssSelector(".btn-default")).click();
 
+        //Assert customer is successfully created
+        assertThat(driver.switchTo().alert().getText(), is("Customer added successfully with customer id :6"));
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+
         Thread.sleep(2000);
     }
 
     @Test
-    public void createBankAccountForCustomer() {
-
+    public void createBankAccountForCustomer() throws InterruptedException {
+        addNewCustomerTest();
+        driver.findElement(By.xpath("//button[contains(text(), 'Open Account')]")).click();
+//        driver.findElement(By.cssSelector("input[placeholder='---Customer Name---']")).;
+        Thread.sleep(2000);
     }
 
     @Test
