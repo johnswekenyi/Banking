8da4540f102c7baa320815c79e9d240818e08eed
johnswekenyi <johnswekenyi@gmail.com>
2022-11-24 18:14:05 +0300

    CustomerActionsTest

---
+++ src/test/java/CustomerActionsTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+public class CustomerActionsTest {
+    @Test
+    public void loginAsCustomerTest() {
+
+    }
+    @Test
+    public void verifyAccountCreatedIsTheSameTest() {
+
+    }
+    @Test
+    public void depositMoneyToAccountAndVerifyTest() {
+
+    }
+    @Test
+    public void withdrawMoneyFromAccountAndVerifyTest() {
+
+    }
+}
