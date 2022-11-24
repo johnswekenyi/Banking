118c78b6ae84a4ed5f0bd4acf505f4e31c9b2d2e
johnswekenyi <johnswekenyi@gmail.com>
2022-11-24 18:06:32 +0300

    - Refactored class - Added tests for BankManagerTest.java

---
+++ src/test/java/BankManagerTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+public class BankManagerTest {
+    @Test
+    public void loginAsBankManagerTest() {
+
+    }
+
+    @Test
+    public void addNewCustomerTest(){
+
+    }
+
+    @Test
+    public void createBankAccountForCustomer() {
+
+    }
+
+    @Test
+    public void deleteCreatedUserTest() {
+
+    }
+}
--- src/test/java/CreateAccountTest.java
+++ src/test/java/CreateAccountTest.java
@@ -1,2 +1,4 @@
+import org.junit.Test;
+
 public class CreateAccountTest {
 }
