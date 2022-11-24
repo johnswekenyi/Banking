8bec644073ffcf1b1b7273c5aa252f05e777aa26
johnswekenyi <johnswekenyi@gmail.com>
2022-11-24 22:08:56 +0300

    - Tear down browser

--- src/main/java/BaseClass.java
+++ src/main/java/BaseClass.java
@@ -1,13 +1,17 @@
+import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseClass {
+    WebDriver driver = new ChromeDriver();
     @Before
     public void setUp() {
-        WebDriver driver = new ChromeDriver();
         driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
         driver.manage().window().maximize();
-        driver.close();
+    }
+    @After
+    public void tearDown() {
+        driver.quit();
     }
 }
